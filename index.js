ab14185e4b6fb99912e5d2ac3c810c50bc2d7e16
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2019-12-01 16:29:04 +0100

    added asnyc way of reading and writing, additionally using callbacks. BTW, I lost my own files so I copied from final files and adjust it to mine by adding or changing some comments

--- index.js
+++ index.js
@@ -7,3 +7,17 @@ const textOut = `This is what we know about avocado: ${textIn}}`;
 fs.writeFileSync('./txt/output.txt', textOut);
 
 // async or non-bloking file reading or writing
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+  if (err) return console.log('could not reach the start file');
+
+  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+    console.log(data2);
+    fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+      console.log(data3);
+
+      fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+        console.log('done successfully');
+      });
+    });
+  });
+});
