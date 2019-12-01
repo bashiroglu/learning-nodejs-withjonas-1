9e9c9ac7d405e9e82ce889fc5183d5453f3f94d1
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2019-12-01 19:19:18 +0100

    added sync reading function which reads data file and send to api request

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
   const pathName = req.url;
   if (pathName === '/' || pathName === '/overview') {
@@ -9,7 +12,10 @@ const server = http.createServer((req, res) => {
   } else if (pathName === '/product') {
     res.end('hi from product');
   } else if (pathName === '/api') {
-    res.end('hi from api');
+    res.writeHead(200, {
+      'Content-type': 'application/json'
+    });
+    res.end(data);
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
