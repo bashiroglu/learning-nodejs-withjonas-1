4795d2ca3323e192191297b104dd17bb8b354968
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2019-12-01 18:56:55 +0100

    added listening different requests

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
+const fs = require('fs');
 const http = require('http');
-
-
-
+const url = require('url');
 
 const server = http.createServer((req, res) => {
-  res.end('hi from server');
+  const pathName = req.url;
+  if (pathName === '/' || pathName === '/overview') {
+    res.end('hi from overview');
+  } else if (pathName === '/product') {
+    res.end('hi from product');
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello world'
+    });
+    res.end('<h1>hi from 404</h1>');
+  }
 });
 server.listen(8000, '127.0.0.1', () => {
   console.log('server created');
