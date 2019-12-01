af6c2eaa41bb36010382df36df1a542b737b1717
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2019-12-01 16:37:21 +0100

    added serverjs file and created server and listen to that server code by using http module

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+
+
+
+const server = http.createServer((req, res) => {
+  res.end('hi from server');
+});
+server.listen(8000, '127.0.0.1', () => {
+  console.log('server created');
+});
