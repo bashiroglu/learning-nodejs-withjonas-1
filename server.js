f8346cb58fcbd79af34de41273dffe27d48dfbe7
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2019-12-01 21:07:39 +0100

    opened modules folder to host modules and replacetemplate function moved there

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const replaceTemplate = require('./modules/replaceTemplate');
 
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template_overview.html`,
@@ -14,21 +15,7 @@ const tempCard = fs.readFileSync(
   `${__dirname}/templates/template_card.html`,
   'utf-8'
 );
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%ID%}/g, product.id);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  //   output = output.replace(/%PRODUCTNAME%/g, product.description);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  if (!product.organic) {
-    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-  }
-  return output;
-};
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
