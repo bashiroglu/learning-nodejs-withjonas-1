9768c89c7c8a6c8ef4f3bf231a23e7389c0c7bf3
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2019-12-02 20:53:39 +0100

    in indexjs file added some functions to understand event loop in node, but didn't work :), just joinking, basically there is patter to follow in node for functions,other think that there is 4 thread which can work similtaneously, sync versions block the whole code, inclucing process.nexttick etc

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 
-
 // sync or bloking file reading or writing
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 
@@ -23,3 +22,10 @@ const fs = require('fs');
 //   });
 // });
 
+setTimeout(() => console.log('Timer 1 finished'), 0);
+setImmediate(() => console.log('Immediate 1 finished'));
+
+fs.readFile('test-file.txt', () => {
+  console.log('I/O finished');
+});
+console.log('top level');
